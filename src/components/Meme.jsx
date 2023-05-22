1774a2e8bc5fd9d19fbd52a93ed38f4ed4fb3ae8
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-05-22 16:41:14 +0530

    first commit

---
+++ src/components/Meme.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Meme() {
+  const [allMemes, setAllMemes] = useState([]);
+
+  const [memeData, setMemeData] = useState({
+    topText: "",
+    bottomText: "",
+    url: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  function generate(e) {
+    e.preventDefault();
+    // console.log(allMemes);
+    let { data } = allMemes;
+    const indx = Math.ceil(Math.random() * data.memes.length);
+    setMemeData((prevData) => {
+      return { ...prevData, url: data.memes[indx].url };
+    });
+    // console.log(memeData.url);
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setMemeData((meme) => {
+      return {
+        ...meme,
+        [name]: value,
+      };
+    });
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("https://api.imgflip.com/get_memes");
+      // console.log(data.data);
+      setAllMemes(data.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={memeData.topText}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={memeData.bottomText}
+          onChange={handleChange}
+          required={true}
+        />
+        <button className="form--button" onClick={generate} type="submit">
+          Get a new meme image 🖼
+        </button>
+      </form>
+      <div className="meme">
+        <img src={memeData.url} className="meme--image" />
+        <h2 className="meme--text top">{memeData.topText}</h2>
+        <h2 className="meme--text bottom">{memeData.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
